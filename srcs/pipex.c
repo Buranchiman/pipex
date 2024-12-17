3eb431ef4dcbc5e58606651ad6ce0d1a1075098a
William <w.vallee4@gmail.com>
2024-12-17 10:21:48 +0100

    ssh this time

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,69 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wivallee <wivallee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chillyd <chillyd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:25:38 by wivallee          #+#    #+#             */
-/*   Updated: 2024/12/13 16:19:56 by wivallee         ###   ########.fr       */
+/*   Updated: 2024/12/16 17:38:12 by chillyd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+int	second_cmd(char **input, int	fd_cmd1)
 {
-	unsigned int	i;
-	unsigned int	j;
-	char			*cat;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	cat = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (cat == NULL)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		cat[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		cat[i + j] = s2[j];
-		j++;
-	}
-	cat[i + j] = '\0';
-	return (cat);
-}
-
-int	second_cmd(char **input)
-{
-	int		fildes[2];
+	int		fd;
+	//int		fildes[2];
+	// char	*buffer;
+	// char	*cmd1 = NULL;
 	char	*env[] = {"PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/opt/bin", NULL};
 	char	*args[] = {"/bin/sh", "-c", input[3], NULL};
 
-	//ft_printf("it's alive");
 	if (access(input[4], W_OK) == -1)
 	{
 		perror(strerror(errno));
 		return (-1);
 	}
-	fildes[0] = STDOUT_FILENO;
-	fildes[1] = open(input[4], O_RDONLY);
-	if (fildes[1] == -1 || fildes[0] == -1)
-	{
-		perror(strerror(errno));
-		return (-1);
-	}
-	pipe(fildes);
-	ft_printf("gnl is %s", get_next_line(1));
+	// fildes[0] = STDOUT_FILENO;
+	fd = open(input[4], O_WRONLY);
+	// if (fildes[1] == -1 || fildes[0] == -1)
+	// {
+	// 	perror(strerror(errno));
+	// 	return (-1);
+	// }
+	//pipe(fildes);
+	// buffer = (char *)ft_calloc(43, sizeof(char));
+	// if (!buffer)
+	// 	return (-1);
+	// while (read(fd_cmd1, buffer, 42) > 0)
+	// 	cmd1 = ft_strjoinfree(cmd1, buffer);
+	// ft_printf("cmd1 is %s\n", cmd1);
+	// close(pipe_fd[0]);
+	// free(buffer);
 	if (fork() == 0)
 	{
-		if (dup2(STDOUT_FILENO, STDIN_FILENO) == -1)
+		if (dup2(fd, STDOUT_FILENO) == -1)
 		{
 			perror("Failed to redirect stdin");
-			close(fildes[1]);
+			//close(fildes[1]);
+			return (-1);
+		}
+		if (dup2(fd_cmd1, STDIN_FILENO) == -1)
+		{
+			perror("Failed to redirect stdin");
+			//close(fildes[1]);
 			return (-1);
 		}
 		if (execve("/bin/sh", args, env) == -1)
@@ -79,7 +67,8 @@ int	second_cmd(char **input)
 }
 int	ft_pipex(char **input)
 {
-	int	fd;
+	int		fd;
+	int		pipe_fd[2];
 	char	*env[] = {"PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/opt/bin", NULL};
 	char	*args[] = {"/bin/sh", "-c", input[2], input[1], NULL};
 	if (access(input[1], R_OK) == -1)
@@ -93,23 +82,34 @@ int	ft_pipex(char **input)
 		perror(strerror(errno));
 		return (-1);
 	}
+	if (pipe(pipe_fd) == -1) 
+	{
+        perror("pipe");
+        return 1;
+    }
 	if (fork() == 0)
 	{
+		close(pipe_fd[0]);
 		if (dup2(fd, STDIN_FILENO) == -1)
 		{
 			perror("Failed to redirect stdin");
 			close(fd);
 			return (-1);
 		}
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1) 
+		{
+            perror("dup2");
+            exit(1);
+        }
+		close(pipe_fd[1]);
 		if (execve("/bin/sh", args, env) == -1)
 		{
 			perror(strerror(errno));
 			return (-1);
 		}
 	}
-	wait(NULL);
-	//ft_printf("still here lol\n");
-	if (second_cmd(input) == -1)
+	close(pipe_fd[1]);
+	if (second_cmd(input, pipe_fd[0]) == -1)
 		return (-1);
 	return (0);
 }
